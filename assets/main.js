6e501d2f41b88a164a63a92267dc4821bcfae973
Arkaprabha Chatterjee <arkoprabhachatterjee@gmail.com>
2018-06-30 23:08:44 +0530

    added Saswata Das' linkedin reference and improved preloader

---
+++ assets/main.js
@@ -0,0 +1,45 @@
+ //Script for preloader
+        
+$(window).on('load', function(){
+    $('.preloader-overlay').fadeOut();
+});
+     
+
+
+//Script for AJAX dynamic page load and class removal on event trigger( viewport width reduction )
+
+$(document).ready(function(){
+            
+            $(window).resize(function() {
+              if ($(window).width()>600 && $(window).width() < 800) {
+                 //$('#menu-footer').removeClass("ml-auto mr-auto"); 
+                 $('.card').removeClass("ml-auto mr-auto"); 
+              }else{
+                  $('.card').addClass("ml-auto mr-auto"); 
+                  
+              }
+            });
+            
+            
+            //Default  
+           $('.section-text').load('assets/includes/steeringCommittee.html');  
+            
+            $('.load-1').click(function(){
+                 $('.section-text').load('assets/includes/steeringCommittee.html');           
+            }); 
+            $('.load-2').click(function(){
+                 $('.section-text').load('assets/includes/editorialCommittee.html');           
+            }); 
+            $('.load-3').click(function(){
+                 $('.section-text').load('assets/includes/advisoryBoard.html');           
+            }); 
+            $('.load-4').click(function(){
+                 $('.section-text').load('assets/includes/organizingCommittee.html');           
+            }); 
+            $('.load-5').click(function(){
+                 $('.section-text').load('assets/includes/technicalProgramme.html');           
+            });  
+            
+            
+});
+    
