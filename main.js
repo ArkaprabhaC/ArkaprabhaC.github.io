4073c6f42b82e32001c43529089ed99419dc8f8c
ArkaprabhaC <arkaprabha.chatterjee31@gmail.com>
2018-10-21 17:00:07 +0530

    second revision update

--- main.js
+++ main.js
@@ -5,6 +5,87 @@ $(window).on('load', function(){
 });
      
 
+//Script for navbar links turning active on click
+$(document).ready(function(){
+  $(".navbar-nav > li ").click(function(){
+      $clickedBtn = $(this);
+      $('.navbar-nav > li').removeClass('active');
+      $clickedBtn.addClass('active');
+  });
+});
+
+//Script for smooth scrolling
+ $(document).ready(function (){
+            $(".home_link").click(function (){
+                $('html, body').animate({
+                    scrollTop: 0
+                }, 1000);
+            });
+            $(".about_btn").click(function (){
+                $('html, body').animate({
+                    scrollTop: $(".about").offset().top - 50
+                }, 1000);
+            });
+           $(".imp_dates_btn").click(function (){
+                $('html, body').animate({
+                    scrollTop: $(".important-dates").offset().top -50
+                }, 1000);
+            });
+           $(".committees_btn").click(function (){
+                $('html, body').animate({
+                    scrollTop: $(".committee").offset().top - 50
+                }, 1000);
+            });
+           $(".reg_details_btn").click(function (){
+                $('html, body').animate({
+                    scrollTop: $(".registration_details").offset().top - 50
+                }, 1000);
+            });
+           $(".past_conf_btn").click(function (){
+                $('html, body').animate({
+                    scrollTop: $(".past_conf").offset().top - 50
+                }, 1000);
+            });
+
+           
+           $(".gall_btn").click(function (){
+                $('html, body').animate({
+                    scrollTop: $(".gallery-block").offset().top - 50
+                }, 1000);
+            });
+           $(".highlights_btn").click(function (){
+                $('html, body').animate({
+                    scrollTop: $(".highlights").offset().top - 50
+                }, 1000);
+            });
+
+           $(".loc_btn").click(function (){
+                $('html, body').animate({
+                    scrollTop: $(".location").offset().top - 50
+                }, 1000);
+            });
+           
+            
+          
+        });
+
+    
+    //Dark background to transparent background navbar conversion
+      $(document).ready(function() {
+        $(window).scroll(function() {
+          if($(this).scrollTop() > 100) { 
+              $('.menu-nav').removeClass('bg-transparent');
+              $('.menu-nav').addClass('bg-dark');
+          } else {
+              $('.menu-nav').removeClass('bg-dark');
+              $('.menu-nav').addClass('bg-transparent');
+              
+          }
+        });
+      });
+    
+
+
 
 //Script for AJAX dynamic page load and class removal on event trigger( viewport width reduction )
 
@@ -33,12 +114,7 @@ $(document).ready(function(){
             $('.load-3').click(function(){
                  $('.section-text').load('assets/includes/advisoryBoard.html');           
             }); 
-            $('.load-4').click(function(){
-                 $('.section-text').load('assets/includes/organizingCommittee.html');           
-            }); 
-            $('.load-5').click(function(){
-                 $('.section-text').load('assets/includes/technicalProgramme.html');           
-            });  
+            
             
 });
     
